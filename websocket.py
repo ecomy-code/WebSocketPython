45366d23380c2bb4e1b6674303cb4de7e8e386d7
Yeisson Araya <80219453+ecomy-code@users.noreply.github.com>
2021-08-17 03:48:25 -0600

    WebSockets
    Se trata de un concepto muy similar al de los sockets TCP/IP y que permite que una aplicación web establezca un canal de comunicación bi-direccional persistente entre la capa de presentación HTML en el browser y el servidor. Los WebSockets utilizan los puertos estándar de http y https (80 y 443 respectivamente) y la especificación fue diseñada para evitar problemas causados por el uso de proxies y de firewalls. Es importante recalcar que aunque los WebSockets usan el puerto 80 al igual que HTTP, usan un protocolo de comunicación distinto, razón por la cual se usa ws:// en la URL en lugar de http://. También es posible encriptar la comunicación usando el protocolo wss://, el cual usará el puerto 443 (el mismo que HTTPS).
    
    Biografía:https://developer.ibm.com/es/articles/que-son-los-websockets/

--- websocket.py
+++ websocket.py
@@ -1,1 +1,20 @@
 #codigo python agosto 2021 semana de websockets
+#pip install "Werkzeug>=2.0.0rc3"
+#linux  FLASK_APP=websocket.py flask run
+#connect un host       wscat --connect http://localhost:5000/revertir_oracion  y escriba algo para revertir
+
+#pip install gunicorn  Multiples conecciones
+#gunicorn -b :5000 --workers 4 --threads 100 websocket:app
+
+from flask import Flask
+from flask_sock import Sock
+
+app = Flask(__name__)
+sock = Sock(app)
+
+@sock.route('/revertir_oracion')
+def revertir (ws):
+  while True:
+    texto = ws.receive()
+    ws.send(texto[::-1])
+
